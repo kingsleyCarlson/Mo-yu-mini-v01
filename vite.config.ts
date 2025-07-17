51def8275ac631e8fb13e52583019cc9f1ad8ebd
kingsleyCarlson <kc_0701@hotmail.com>
2025-07-18 01:22:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-error-overlay";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-watch").then((m) =>
-            m.cartographer()
-          ),
-        ]
-      : []),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    outDir: "dist", // 明确告诉 Vite 输出目录
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html",
+    },
   },
 });
